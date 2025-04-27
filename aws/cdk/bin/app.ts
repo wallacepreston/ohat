03cb70c600cf5b4b4b2dcb19c1572a9d6f4c2ba1
Preston Wallace <wallace.preston@gmail.com>
2025-04-27 11:58:15 -0700

    bootstrap

--- aws/cdk/bin/app.ts
+++ aws/cdk/bin/app.ts
@@ -3,17 +3,17 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SqsLambdaStack } from '../lib/sqs-lambda-stack';
 
+// Get account and region from environment variables or use defaults
+const account = process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT;
+const region = process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION || process.env.AWS_REGION;
+
 const app = new cdk.App();
 new SqsLambdaStack(app, 'SqsLambdaStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  /* Use explicit environment settings to ensure we deploy to the correct region */
+  env: { 
+    account,
+    region
+  },
 });
 
 app.synth(); 
